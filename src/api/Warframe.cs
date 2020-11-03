27aec1325cb885bf875da4ff92523cfc6042e828
Dakota Ruelle <druelle@omnitech-inc.com>
2020-11-03 12:35:45 -0600

    Add a web api project

---
+++ src/api/Warframe.cs
@@ -0,0 +1,7 @@
+namespace Api
+{
+  public class Warframe
+  {
+    public string Name { get; set; }
+  }
+}
---
+++ src/api/WarframesController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Api.Controllers
+{
+  [ApiController]
+  [Route("warframes")]
+  public class WarframesController : ControllerBase
+  {
+    private readonly ILogger<WarframesController> _logger;
+
+    public WarframesController(ILogger<WarframesController> logger)
+    {
+      _logger = logger;
+    }
+
+    [HttpGet]
+    public IEnumerable<Warframe> Get()
+    {
+      var warFrames = new List<Warframe>
+            {
+              new Warframe
+              {
+                Name = "Ash"
+              },
+              new Warframe
+              {
+                Name = "Ember"
+              }
+            };
+
+      return warFrames;
+    }
+  }
+}
