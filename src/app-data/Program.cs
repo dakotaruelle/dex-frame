da922568fb46f016d4c5dfe757fe4ec1a1c49432
Dakota Ruelle <17259752+dakotaruelle@users.noreply.github.com>
2020-11-20 07:25:11 -0600

    Add database project with DbUp

---
+++ src/app-data/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using DbUp;
+
+namespace AppData
+{
+  class Program
+  {
+    static int Main(string[] args)
+    {
+      var connectionString = args.FirstOrDefault() ?? "Server=localhost\\SqlExpress; Database=FrameDex; Trusted_connection=true";
+      EnsureDatabase.For.SqlDatabase(connectionString);
+
+      var upgrader = DeployChanges.To
+        .SqlDatabase(connectionString)
+        .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+        .LogToConsole()
+        .Build();
+
+      var result = upgrader.PerformUpgrade();
+
+      if (!result.Successful)
+      {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(result.Error);
+        Console.ResetColor();
+
+        // #if DEBUG
+        //   Console.ReadLine();
+        // #endif
+
+        return -1;
+      }
+
+      Console.ForegroundColor = ConsoleColor.Green;
+      Console.WriteLine("Success!");
+      Console.ResetColor();
+
+      return 0;
+    }
+  }
+}
