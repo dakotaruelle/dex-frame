4cbc229b8cb2db82e95eaa97e9900afdbce0672e
Dakota Ruelle <17259752+dakotaruelle@users.noreply.github.com>
2020-12-20 08:49:19 -0600

    Update authority name for IdentityServer

--- src/web-client/src/Startup.cs
+++ src/web-client/src/Startup.cs
@@ -29,7 +29,7 @@ namespace WebClient
         .AddCookie("Cookies")
         .AddOpenIdConnect("oidc", options =>
         {
-          options.Authority = "https://localhost:8001";
+          options.Authority = "https://framedex-identity.azurewebsites.net/";
 
           options.ClientId = "interactive";
           options.ClientSecret = "49C1A7E1-0C79-4A89-A3D6-A37998FB86B0";
